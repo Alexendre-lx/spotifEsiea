ab13ad26b60522e663f2d49600cc85794643e239
Alexendre-lx <alexendre.obli@gmail.com>
2019-12-14 15:35:47 +0100

    first commit

--- SpotifEsiea/ViewController.swift
+++ SpotifEsiea/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
